cbca5249f6ef7dc1691206f80af175264c8b238d
Napatsorn12 <napassor555@gmail.com>
2024-09-05 15:17:14 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,5 @@
+console.log('this is my script file from javascript')
+document.getElementById("text").innerHTML = "Napatsorn Nanum";
+document.write("Hello document write");
+
+window.alert("welcome to my page");
