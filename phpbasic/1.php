90a4e6ca4c3eca1379253ac2906b243516d76274
Napatsorn12 <napassor555@gmail.com>
2024-09-12 15:21:24 +0700

    Add files via upload

---
+++ phpbasic/1.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Hello PHP</title>
+</head>
+<body>
+    <?php
+        echo "Hello PHP";
+        echo "<br>";
+        echo "this is my first time to write PHP Language";
+        echo "<br>";
+        echo "<br>";
+        define("MINSIZE",50);
+        echo MINSIZE;
+    ?>
+</body>
+</html>
---
+++ phpbasic/function26.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WriteMessage</title>
+</head>
+<body>
+    <?php
+    function writeMessage(){
+        echo "this is first fumction for write message";
+        echo "Have a nice program":
+    }//stop function
+
+    //call fumction
+    writeMessage();
+    ?>
+</body>
+</html>
